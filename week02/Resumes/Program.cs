a4d3c1c7f83ce7860bd0fcec7b6b915d6f1fb782
bostler3 <bostler3@gmail.com>
2025-01-14 07:20:37 -0700

    Week 2 Learning Activity-Abstraction - Resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Comment these two lines if the Display() methods from below work (they are a progress check)
+        // Console.WriteLine(job1._company);
+        // Console.WriteLine(job2._company);
+
+        // Comment the following two lines if the myResume.Display() call below works (they are a progress check)
+        // job1.Display();
+        // job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Comment the following line if it works (it's a progress check)
+        // Console.WriteLine(myResume._jobs[0]._jobTitle);
+
+        myResume.Display();
     }
 }
