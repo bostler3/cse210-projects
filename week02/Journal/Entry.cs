ac3c89164434cdbc81569f31ed439c12be322165
bostler3 <bostler3@gmail.com>
2025-01-16 07:19:01 -0700

    Week 2 Team Activity Initial Framework for Journal Assignment

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,13 @@
+public class Entry
+{
+    // Assignment instructions say the date can be a string, but consider trying to make it a date object
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_date} {_promptText}");
+        Console.WriteLine(_entryText);
+    }
+}
